c13df73a721e2c9877a63d4213f3c9fc80bb3905
HW, Siew <hw.siew@actiweb.co>
2021-04-27 16:17:36 +0800

    Added: babel support in webpack

--- webpack.admin.js
+++ webpack.admin.js
@@ -15,10 +15,20 @@ module.exports = (env) => {
 		},
 		module: {
 			rules: [
-			  {
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			  },
+				{
+					test: /\.m?js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+					  loader: 'babel-loader',
+					  options: {
+						presets: ['@babel/preset-env']
+					  }
+					}
+				},
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
 			],
 		},
 	};
