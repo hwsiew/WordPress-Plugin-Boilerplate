81085c83a44127ffcf6de209d16d525caf8d08dc
HW, Siew <hw.siew@actiweb.co>
2021-04-27 16:43:55 +0800

    Updated: refactor webpack configs and support production build

--- webpack.admin.js
+++ webpack.admin.js
@@ -1,11 +1,15 @@
-const path = require('path');
+const path 		= require('path');
+const { merge } = require('webpack-merge');
+const common 	= require('./webpack.common.js');
 
 require('dotenv').config();
 
 module.exports = (env) => {
-	
-	return {
-		mode: 'development',
+
+	let mode = env.production ? 'production' : 'development';
+
+	return merge(common, {
+		mode: mode,
 		entry: {
 			admin: `./${process.env.SLUG}/admin/src/${process.env.SLUG}-admin.js`,
 		},
@@ -13,23 +17,38 @@ module.exports = (env) => {
 			filename: `[name].bundle.js`,
 			path: path.resolve(__dirname, `${process.env.SLUG}/admin/dist`),
 		},
-		module: {
-			rules: [
-				{
-					test: /\.m?js$/,
-					exclude: /(node_modules|bower_components)/,
-					use: {
-					  loader: 'babel-loader',
-					  options: {
-						presets: ['@babel/preset-env']
-					  }
-					}
-				},
-				{
-					test: /\.css$/i,
-					use: ['style-loader', 'css-loader'],
-				},
-			],
-		},
-	};
+	})
+
 };
+
+// module.exports = (env) => {
+	
+// 	return {
+// 		mode: 'development',
+// 		entry: {
+// 			admin: `./${process.env.SLUG}/admin/src/${process.env.SLUG}-admin.js`,
+// 		},
+// 		output: {
+// 			filename: `[name].bundle.js`,
+// 			path: path.resolve(__dirname, `${process.env.SLUG}/admin/dist`),
+// 		},
+// 		module: {
+// 			rules: [
+// 				{
+// 					test: /\.m?js$/,
+// 					exclude: /(node_modules|bower_components)/,
+// 					use: {
+// 					  loader: 'babel-loader',
+// 					  options: {
+// 						presets: ['@babel/preset-env']
+// 					  }
+// 					}
+// 				},
+// 				{
+// 					test: /\.css$/i,
+// 					use: ['style-loader', 'css-loader'],
+// 				},
+// 			],
+// 		},
+// 	};
+// };
--- webpack.public.js
+++ webpack.public.js
@@ -1,11 +1,15 @@
-const path = require('path');
+const path 		= require('path');
+const { merge } = require('webpack-merge');
+const common 	= require('./webpack.common.js');
 
 require('dotenv').config();
 
 module.exports = (env) => {
-	
-	return {
-		mode: 'development',
+
+	let mode = env.production ? 'production' : 'development';
+
+	return merge(common, {
+		mode: mode,
 		entry: {
 			public: `./${process.env.SLUG}/public/src/${process.env.SLUG}-public.js`,
 		},
@@ -13,23 +17,38 @@ module.exports = (env) => {
 			filename: `[name].bundle.js`,
 			path: path.resolve(__dirname, `${process.env.SLUG}/public/dist`),
 		},
-		module: {
-			rules: [
-				{
-					test: /\.m?js$/,
-					exclude: /(node_modules|bower_components)/,
-					use: {
-					  loader: 'babel-loader',
-					  options: {
-						presets: ['@babel/preset-env']
-					  }
-					}
-				},
-				{
-					test: /\.css$/i,
-					use: ['style-loader', 'css-loader'],
-				},
-			],
-		},
-	};
+	})
+
 };
+
+// module.exports = (env) => {
+	
+// 	return {
+// 		mode: 'development',
+// 		entry: {
+// 			public: `./${process.env.SLUG}/public/src/${process.env.SLUG}-public.js`,
+// 		},
+// 		output: {
+// 			filename: `[name].bundle.js`,
+// 			path: path.resolve(__dirname, `${process.env.SLUG}/public/dist`),
+// 		},
+// 		module: {
+// 			rules: [
+// 				{
+// 					test: /\.m?js$/,
+// 					exclude: /(node_modules|bower_components)/,
+// 					use: {
+// 					  loader: 'babel-loader',
+// 					  options: {
+// 						presets: ['@babel/preset-env']
+// 					  }
+// 					}
+// 				},
+// 				{
+// 					test: /\.css$/i,
+// 					use: ['style-loader', 'css-loader'],
+// 				},
+// 			],
+// 		},
+// 	};
+// };
